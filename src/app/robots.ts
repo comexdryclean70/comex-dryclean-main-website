db2a11572704d236d30b5dc9d46f5c3c1f09e2bd
robin gautam <robingautam@mittals-macbook.local>
2026-02-04 00:55:11 +0530

    fix: align robots.txt with user specification

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
-            disallow: '/private/', // Block admin or private routes if you have them
         },
         sitemap: 'https://comexdrycleaner.com/sitemap.xml',
     }
